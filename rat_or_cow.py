e1bdbde1c5d1ef55f3e0616ffbb1907e4aba3e77
John Scott <jscottjscott@gmail.com>
2021-07-18 13:39:58 -0700

    first version of rat or cow code

---
+++ rat_or_cow.py
@@ -0,0 +1,7 @@
+c = input("which is a better animal- rat or cow?")
+if c == 'cow':
+	print('cow it is')
+elif c == 'rat':
+	print("rats aren't as cool but okay")
+else:
+	print('rats and cows only, please!')
